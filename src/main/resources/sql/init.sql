62b0d8e0f6ad4db2eea4f45b0ff341595815e2fa
guoxiang <18841845137@163.com>
2021-07-28 15:53:03 +0800

    [UPD] 增加测试数据

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -10,7 +10,7 @@ create table if not exists rule_demo.tb_rule
 )
 comment '规则';
 
-INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (28, 'test', 'package rules
+INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (null, 'test', 'package rules
 
 import com.fly.drools.entity.RuleResult
 import org.slf4j.Logger
@@ -50,7 +50,7 @@ rule "discount_rule_4"
         List<Rule> list = jdbc.query("select * from tb_rule", new BeanPropertyRowMapper<>(Rule.class));
         log.info("rule list = {}", list);
 end', '2021-07-27 09:34:16', '2021-07-28 11:35:29', '阿凡达尬舞人噶为如果阿瓦噶人噶微软官方啊我日尬舞人噶为啊我发噶违法啊哥哥二哥奥尔格如果sergeA而是如果奥尔格色弱各色如果而是各色染个色染个色如果色弱各色如果QWEFGAERGERGERG ERSG');
-INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (29, 'test', 'package rules
+INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (null, 'test', 'package rules
 
 import com.fly.drools.entity.RuleResult
 import org.slf4j.Logger
@@ -84,7 +84,7 @@ rule "discount_rule_4"
         }
         r.setData("成人");
 end', '2021-07-27 09:34:17', '2021-07-28 12:59:18', null);
-INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (30, '页面测试规则1', 'package rules
+INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (null, '页面测试规则1', 'package rules
 
 import com.fly.drools.entity.RuleResult
 import org.slf4j.Logger
@@ -124,7 +124,7 @@ rule "discount_rule_4"
         List<Rule> list = jdbc.query("select * from tb_rule", new BeanPropertyRowMapper<>(Rule.class));
         log.info("rule list = {}", list);
 end', '2021-07-28 13:06:22', '2021-07-28 13:06:22', '这是一个测试规则');
-INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (31, '测试2', 'package rules
+INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (null, '测试2', 'package rules
 
 import com.fly.drools.entity.RuleResult
 import org.slf4j.Logger
@@ -164,7 +164,7 @@ rule "discount_rule_4"
         List<Rule> list = jdbc.query("select * from tb_rule", new BeanPropertyRowMapper<>(Rule.class));
         log.info("rule list = {}", list);
 end', '2021-07-28 13:13:12', '2021-07-28 13:14:46', '测试2');
-INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (32, '测试3', 'package rules
+INSERT INTO rule_demo.tb_rule (id, name, rule_text, create_time, update_time, description) VALUES (null, '测试3', 'package rules
 
 import com.fly.drools.entity.RuleResult
 import org.slf4j.Logger
