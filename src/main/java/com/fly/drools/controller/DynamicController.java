c2451f137c51a3720293a776e1fa8941b16d459d
guoxiang <18841845137@163.com>
2021-07-28 09:55:04 +0800

    [UPD] 优化代码

--- src/main/java/com/fly/drools/controller/DynamicController.java
+++ src/main/java/com/fly/drools/controller/DynamicController.java
@@ -1,17 +1,11 @@
 package com.fly.drools.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fly.drools.entity.RuleResult;
 import com.fly.drools.service.DynamicService;
-import com.fly.drools.service.RuleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.kie.api.definition.KiePackage;
-import org.kie.api.definition.type.FactType;
-import org.kie.api.runtime.KieSession;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.Map;
 
 /**
